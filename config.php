6a301ba21e41dc193bfde0c418f98d3e7d6a8fc8
VINICIUS FERREIRA <viniciusfe66@gmail.com>
2021-09-13 12:32:44 -0400

    banco

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php 
 
-$nome = 'Encontrei lá brecho';
-$email = 'encontrei.labrecho@gmail.com';
+$nome = 'Encontrei!';
+$email = 'contato@encontrei.store';
 $telefone = '65 992620294';
 
 
 //VARIAVEIS BANCO DE DADOS
-$servidor = 'localhost';
-$usuario = 'root';
-$senha = '';
-$banco = 'loja';
+$servidor = 'mysql380.umbler.com';
+$usuario = 'vinicius';
+$senha = '15987vini';
+$banco = 'loja_encontrei';
 
 
 ?>
