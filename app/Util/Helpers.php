8636e49e19a383ea0743d331750b96776e43dc4b
Vinicius Ferreira <Viniciusfe66@gmail.com>
2024-10-16 22:01:23 -0400

    feat: crição de herlper validar CPF

--- app/Util/Helpers.php
+++ app/Util/Helpers.php
@@ -12,4 +12,28 @@ class Helpers {
         }
         return number_format(0, 2, ',', '.');
     }
+
+    static function validarCPF($cpf) {
+        // Remove qualquer máscara de formatação como pontos e traços
+        $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
+
